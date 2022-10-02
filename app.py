b87477aeea756ecfc5faed1a027d8c08d980ed09
sidboricha26 <83353666+sidboricha26@users.noreply.github.com>
2022-10-02 11:29:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,21 +13,17 @@ st.title('Potato Leaf Disease Prediction')
 def main() :
     file_uploaded = st.file_uploader('Choose an image...', type = 'jpg')
     if file_uploaded is not None :
-       # image = Image.open(file_uploaded)
         image = image.open(file_uploaded)
         st.write("Uploaded Image.")
         figure = plt.figure()
-       # plt.imshow(image)
         plt.show(image)
         plt.axis('off')
-        #st.pyplot(figure)
         result, confidence = predict_class(image)
         st.write('Prediction : {}'.format(result))
         st.write('Confidence : {}%'.format(confidence))
 
 def predict_class(image) :
     with st.spinner('Loading Model...'):
-       # classifier_model = keras.models.load_model(r'potatoes.h5', compile = False)
        classifier_model = keras.models.load_model('potatoes.h5', compile = False)
 
     shape = ((256,256,3))
